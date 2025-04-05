797eeed776fb6c89581690dbb0f569989a5c6698
Ma Alejandra Carrillo <ma.carrillo2@uniandes.edu.co>
2025-04-05 11:12:26 -0500

    Cambios finales

--- main.ts
+++ main.ts
@@ -10,17 +10,39 @@ promedioTempo.innerHTML = `${getPromedioTemporadas(series)}`
 function renderSeriesInTable(series: Serie[]):void{
     series.forEach(c => {
         let trElement = document.createElement("tr");
-        trElement.innerHTML = `<td>${c.id}</td>
-                               <td>${c.nombre}</td>
-                               <td>${c.medioDifusion}</td>
-                               <td>${c.temporadas}</td>`;
+        trElement.innerHTML = `
+            <td>${c.id}</td>
+            <td id="nombre-${c.id}"><a href="#">${c.nombre}</a></td>
+            <td>${c.medioDifusion}</td>
+            <td>${c.temporadas}</td>
+        `;
+        let nombreCell = trElement.querySelector(`#nombre-${c.id}`);
+        if (nombreCell) {
+            nombreCell.addEventListener('click', (event) => {
+                mostrarTarjeta(c);  
+            });
+        }
         seriesTbody.appendChild(trElement);
     })
 }
-
+//
 function getPromedioTemporadas(series: Serie[]): number {
     let totalTemp: number = 0;
     let promedio: number = 0;
     series.forEach((serie) => totalTemp = totalTemp + serie.temporadas);
     return totalTemp/series.length;
+}
+
+function mostrarTarjeta(serie:Serie){
+    const serieCard = document.getElementById('serie-card') as HTMLDivElement;
+    const serieTitle = document.getElementById('titulo-card') as HTMLHeadingElement;
+    const serieImg = document.getElementById('imagen-card') as HTMLImageElement;
+    const serieSynopsis = document.getElementById('sinopsis-card') as HTMLParagraphElement;
+    const serieLink = document.getElementById('url-card') as HTMLAnchorElement;
+
+    serieTitle.textContent = serie.nombre;
+    serieImg.src = serie.imagen;
+    serieSynopsis.textContent = serie.sinopsis;
+    serieLink.href = serie.url;
+    serieCard.style.display = 'block';
 }
--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,32 @@ promedioTempo.innerHTML = "".concat(getPromedioTemporadas(series));
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                               <td>").concat(c.nombre, "</td>\n                               <td>").concat(c.medioDifusion, "</td>\n                               <td>").concat(c.temporadas, "</td>");
+        trElement.innerHTML = "\n            <td>".concat(c.id, "</td>\n            <td id=\"nombre-").concat(c.id, "\"><a href=\"#\">").concat(c.nombre, "</a></td>\n            <td>").concat(c.medioDifusion, "</td>\n            <td>").concat(c.temporadas, "</td>\n        ");
+        var nombreCell = trElement.querySelector("#nombre-".concat(c.id));
+        if (nombreCell) {
+            nombreCell.addEventListener('click', function (event) {
+                mostrarTarjeta(c);
+            });
+        }
         seriesTbody.appendChild(trElement);
     });
 }
+//
 function getPromedioTemporadas(series) {
     var totalTemp = 0;
     var promedio = 0;
     series.forEach(function (serie) { return totalTemp = totalTemp + serie.temporadas; });
     return totalTemp / series.length;
+}
+function mostrarTarjeta(serie) {
+    var serieCard = document.getElementById('serie-card');
+    var serieTitle = document.getElementById('titulo-card');
+    var serieImg = document.getElementById('imagen-card');
+    var serieSynopsis = document.getElementById('sinopsis-card');
+    var serieLink = document.getElementById('url-card');
+    serieTitle.textContent = serie.nombre;
+    serieImg.src = serie.imagen;
+    serieSynopsis.textContent = serie.sinopsis;
+    serieLink.href = serie.url;
+    serieCard.style.display = 'block';
 }
